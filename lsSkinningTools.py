2e09e48f336efcf63754fb80c1ada98bf43a371a
leonsooi <leonsooi@gmail.com>
2013-07-28 11:08:29 -0400

    Initial test release

---
+++ lsSkinningTools.py
@@ -0,0 +1,16 @@
+############################################
+# GET WEIGHTS OF A JOINT
+############################################
+# select joint, then mesh
+bndJnt, mesh = mc.ls(os=True)[:2]
+
+vtxCount = mc.polyEvaluate(mesh, v=True)
+skn = meval('findRelatedSkinCluster("%s")' % mesh)
+
+weights = []
+
+for vtxId in range(vtxCount):
+	weight = mc.skinPercent(skn, '%s.vtx[%d]' % (mesh, vtxId), transform=bndJnt, q=True, v=True)
+	weights.append(weight)
+	
+print weights
